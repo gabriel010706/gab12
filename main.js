5b477e5118df15caa207af53e5619f0ce7506a8e
gabriel010706 <128716288+gabriel010706@users.noreply.github.com>
2023-10-17 11:09:03 -0300

    ok

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ function tocaSomPom(){
     document.querySelector("#som_tecla_pom").play();
 }
 //chamando/invocando a função tocaSomPom pelo clique do botão pom
-document.querySelector(".tecla_pom").onclick=tocaSomPom;
-//criando contante listaDeTeclas buscando
-//todos os itens pela classe.tecla
+document.querySelector(".tecla_pom").onclick=tocaSomPom; 
+
+//criando contante listaDeTeclas buscando todos os itens pela classe.tecla
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 //incocando a função tocaSomPom a partir do item '0'  , 1° botão
