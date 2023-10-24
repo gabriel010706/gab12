2c1ff1fb3762eb1886eafd39a910e69f97768086
gabriel010706 <128716288+gabriel010706@users.noreply.github.com>
2023-10-24 10:58:36 -0300

    ok

--- main.js
+++ main.js
@@ -8,12 +8,16 @@ document.querySelector(".tecla_pom").onclick=tocaSomPom;
 //criando contante listaDeTeclas buscando todos os itens pela classe.tecla
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
-//incocando a função tocaSomPom a partir do item '0'  , 1° botão
+//incocando a função tocaSomPom a partir do item '0'  , 1° botão.
 listaDeTeclas[0].onclick = tocaSomPom;
 
 
 //criando referência variável 'contador' iniciando = 0
 let contador =0;
-while(){
-//estrutura de repetiçao 'while' significa
+
+//estrutura de repetiçao 'while' significa"enquanto algo acontecer"
+while(contador < 9){
+listaDeTeclas[contador].onclick = tocaSomPom;
+contador = contador +1;
+console.log(contador);
 }
